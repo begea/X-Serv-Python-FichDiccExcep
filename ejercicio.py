07045b5425dc3d486e805a12aaac0563d78c82fd
Borja Egea Madrid <begea@zeta40.aulas.gsyc.es>
2016-02-10 10:54:33 +0100

    He hecho el ejercicio 13.5

--- ejercicio.py
+++ ejercicio.py
@@ -1,13 +1,27 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-# hola, hola
-
-fd = open('/etc/passwd', 'r')
+fd = open("/etc/passwd", "r")
 
 lineas = fd.readlines()
+diccionario_usuarios = {}
 fd.close()
 
 for linea in lineas:
-    elementos = linea.split(':')
-    print elementos[0], elementos[-1][:-1]
+	trozos = linea.split(':')
+	usuario = trozos[0]
+	shell = trozos[-1]
+	diccionario_usuarios[usuario] = shell
+
+nombre_usu1 = 'root'
+nombre_usu2 = 'imaginario'
+
+nombre_shell1 = diccionario_usuarios[nombre_usu1]
+print "Usuario:" + nombre_usu1, " Shell:" + nombre_shell1[0:-1]
+
+try:
+	nombre_shell2 = diccionario_usuarios[nombre_usu2]
+	print "Usuario:", nombre_usu2, " Shell:", nombre_shell2[0:-1]
+	
+except KeyError:
+	print "No se ha podido encontrar el usuario: " + "'" + nombre_usu2 + "'"
